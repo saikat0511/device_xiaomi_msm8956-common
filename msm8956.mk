de8a34ad7b25512134b531d321a3dda356de10e2
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-21 20:29:24 +0200

    Revert "msm8956-common: build power stats hal"
    This reverts commit eef135bb373da7e9eedce8fb89ca3b00b4de924f.

--- msm8956.mk
+++ msm8956.mk
@@ -338,8 +338,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.2-service-qti
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
